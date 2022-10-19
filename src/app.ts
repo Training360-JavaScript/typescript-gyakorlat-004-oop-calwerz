8755a268aa1bdddd20b27a49c78abec4a67d0f91
Daniel Biro <Daniel.Biro@evosoft.com>
2022-10-19 15:59:39 +0200

    4/4

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 // Importáld be a HumanHero és TransformerHero osztályokat.
 
+import { HumanHero } from "./hero";
+import { TransformerHero } from "./transformer";
+
 
 /**
  * Állítsd be helyesen a változót!
@@ -8,7 +11,11 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+export const humans: HumanHero[] = [
+    new HumanHero(1, "Nyx", "female", 22, 75),
+    new HumanHero(2, "Visor", "male", 34, 100),
+    new HumanHero(3, "Keel", "male", 27, 125),
+];
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +24,8 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} transformers
  */
-export const transformers: any[] = [];
+export const transformers: TransformerHero[] = [
+    new TransformerHero(11, "Trans János", 1, 12, "Audi"),
+    new TransformerHero(12, "Trans Ferenc", 1, 12, "VW"),
+    new TransformerHero(13, "Trans Béla", 1, 12, "Porsche"),
+];
